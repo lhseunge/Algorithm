db35a29953d5d91c9ef73fde03763646426c5514
lhseunge <107404774+lhseunge@users.noreply.github.com>
2024-03-10 01:21:16 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -4,9 +4,9 @@ class Solution {
         String[] parts = binomial.split(" ");
         
         switch (parts[1]) {
-            case "+" : return Integer.parseInt(parts[0]) + Integer.parseInt(parts[2]);
-            case "-" : return Integer.parseInt(parts[0]) - Integer.parseInt(parts[2]);
-            case "*" : return Integer.parseInt(parts[0]) * Integer.parseInt(parts[2]);
+            case "+" : return Math.addExact(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]));
+            case "-" : return Math.subtractExact(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]));
+            case "*" : return Math.multiplyExact(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]));
         }        
 
         return 0;
