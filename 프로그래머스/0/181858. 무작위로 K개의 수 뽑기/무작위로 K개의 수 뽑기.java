6ff56e7b7645542ebb46d3edab8a6135b41607b5
lhseunge <107404774+lhseunge@users.noreply.github.com>
2024-03-15 23:59:03 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 11.32 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,11 +1,8 @@
 import java.util.Arrays;
 
 class Solution {
-    
-    int[] answer = {};
-    
     public int[] solution(int[] arr, int k) {
-        answer = new int[k];
+        int[] answer = new int[k];
         Arrays.fill(answer, -1);
         
         arr = Arrays.stream(arr)
@@ -14,18 +11,21 @@ class Solution {
         
         
         if (k > arr.length) {
-            arrMap(answer, arr, arr.length);
+            answer = arrMap(answer, arr, arr.length);
         } else {
-            arrMap(answer, arr, k);
+            answer = arrMap(answer, arr, k);
         }
+        
+        
         
         return answer;
     }
     
-    private void arrMap(int[] answer, int[] fillArr, int idx) {
+    private int[] arrMap(int[] answer, int[] fillArr, int idx) {
+        for (int i = 0; i < idx; i++) {
+                answer[i] = fillArr[i];
+        }
         
-        for (int i = 0; i < idx; i++) 
-            answer[i] = fillArr[i];
-        
+        return answer;
     }
 }
