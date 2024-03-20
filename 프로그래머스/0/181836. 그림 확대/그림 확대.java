6803e0d3ecaf72e4634f37e96d328533afd3e8d0
lhseunge <107404774+lhseunge@users.noreply.github.com>
2024-03-21 00:10:54 +0900

    [level 0] Title: 그림 확대, Time: 3.10 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,18 +1,22 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public String[] solution(String[] picture, int k) {
         String[] answer = new String[picture.length * k];
+        String temp;
         int idx = 0;
         
         for (String s : picture) {
+            // temp += String.valueOf(s.);
             for (int i = 0; i < k; i++) {
-                answer[idx] = String.join("", Arrays.stream(s.split(""))
+                answer[idx] = Arrays.stream(s.split(""))
                                           .map(x -> x = x.repeat(k))
-                                          .toArray(String[]::new));   
+                                          .collect(Collectors.joining());   
                 
                 idx++;
             }             
+            
         }
         
         return answer;
