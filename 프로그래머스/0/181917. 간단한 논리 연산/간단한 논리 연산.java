46b4526a417570aa73cc07fbd119eec150a28414
lhseunge <107404774+lhseunge@users.noreply.github.com>
2024-03-28 00:00:03 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-
-        Boolean case1 = Boolean.logicalOr(x1, x2);
-        Boolean case2 = Boolean.logicalOr(x3, x4);
-        
-        return Boolean.logicalAnd(case1, case2);
+        return (x1 || x2) && (x3 || x4);
     }
 }
